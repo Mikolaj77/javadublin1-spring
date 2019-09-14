eed286ec1e5ccfc431b931ebe43eb3ad48e51fc7
mikolaj77 <mikolajh1@gmail.com>
2019-09-14 15:18:56 +0100

    Users

--- src/main/java/pl/sda/javadublin1spring/HelloBean.java
+++ src/main/java/pl/sda/javadublin1spring/HelloBean.java
@@ -1,4 +1,17 @@
 package pl.sda.javadublin1spring;
 
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
 public class HelloBean {
+
+    public void hello() {
+        System.out.println("Hello World from hello method ");
+    }
+    @PostConstruct
+    public void init() {
+        System.out.println("Messade from HelloBean postConstruck");
+    }
 }
---
+++ src/main/java/pl/sda/javadublin1spring/user/InMemoryUserRepository.java
@@ -0,0 +1,48 @@
+package pl.sda.javadublin1spring.user;
+
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Primary
+@Component
+public class InMemoryUserRepository implements UserRepository{
+    private List<User> users;
+
+    public InMemoryUserRepository(){
+        this.users = new ArrayList<>();
+    }
+
+    InMemoryUserRepository(List<User> users) {
+        this.users = users;
+    }
+    @Override
+
+    public Optional<User> findById(Long id) {
+        if (id == null || id <= 0){
+            throw new IllegalArgumentException();
+        }
+        return users.stream()
+                .filter(user -> id.equals(user.getId()))
+                .findFirst();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users);
+    }
+    @PostConstruct
+    private void init() {
+
+        this.users.add(new User(1L, "Szymon", "Nowak", Gender.MALE));
+        this.users.add(new User(2L, "Jan", "Kowalski", Gender.MALE));
+        this.users.add(new User(3L, "Anna", "Wiśniewska", Gender.FEMALE));
+        this.users.add(new User(4L, "Karolina", "Nowak", Gender.FEMALE));
+
+    }
+}
